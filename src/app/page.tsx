ef64cd58dd651b8e3a49f5d39c163b2f991d4459
Not TCA <realtcatech@gmail.com>
2023-09-03 20:10:00 +0800

    actual homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,49 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1>hello world</h1>;
+  return (
+    <>
+      <h1 className="text-lg font-light">projects</h1>
+      <ul className="mt-2.5 flex flex-col gap-3">
+        <li>
+          <Link href="https://yabcounts.com" className="flex flex-col gap-1.5">
+            <h1 className="font-medium underline underline-offset-4">
+              yabcounts
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              live counters, reimagined.
+            </p>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="https://statistical.vercel.app"
+            className="flex flex-col gap-1.5"
+          >
+            <h1 className="font-medium underline underline-offset-4">
+              statistical
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              tracking your favorite creator&apos;s statistics so you can see
+              their growth.
+            </p>
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="https://nickify.vercel.app"
+            className="flex flex-col gap-1.5"
+          >
+            <h1 className="font-medium underline underline-offset-4">
+              nickify
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              in one command, you can make your subscriber count visible in your
+              nickname.
+            </p>
+          </Link>
+        </li>
+      </ul>
+    </>
+  );
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
+import * as React from "react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex w-full items-center justify-between py-6">
+      <Link href="/">
+        <h1 className="text-lg font-semibold">ToastedToast</h1>
+        <p className="text-xs">frontend & discord bot developer</p>
+      </Link>
+      <div>
+        <ModeToggle />
+      </div>
+    </nav>
+  );
+}
+
+function ModeToggle() {
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
