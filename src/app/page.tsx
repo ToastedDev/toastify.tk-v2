ba5829ceaa0c6f228cd80fd404ac9df24c6122fa
Not TCA <realtcatech@gmail.com>
2023-09-04 09:43:07 +0800

    improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,47 @@
+import { ArrowTopRightIcon } from "@radix-ui/react-icons";
 import Link from "next/link";
+
+function Project({
+  name,
+  description,
+  href,
+}: {
+  name: string;
+  description: string;
+  href: string;
+}) {
+  return (
+    <li className="flex flex-col gap-1">
+      <Link href={href}>
+        <h1 className="flex items-center gap-0.5 font-medium underline underline-offset-4">
+          {name} <ArrowTopRightIcon />
+        </h1>
+      </Link>
+      <p className="text-sm text-muted-foreground">{description}</p>
+    </li>
+  );
+}
 
 export default function Home() {
   return (
     <>
       <h1 className="text-lg font-light">projects</h1>
       <ul className="mt-2.5 flex flex-col gap-3">
-        <li>
-          <Link href="https://yabcounts.com" className="flex flex-col gap-1.5">
-            <h1 className="font-medium underline underline-offset-4">
-              yabcounts
-            </h1>
-            <p className="text-sm text-muted-foreground">
-              live counters, reimagined.
-            </p>
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="https://statistical.vercel.app"
-            className="flex flex-col gap-1.5"
-          >
-            <h1 className="font-medium underline underline-offset-4">
-              statistical
-            </h1>
-            <p className="text-sm text-muted-foreground">
-              tracking your favorite creator&apos;s statistics so you can see
-              their growth.
-            </p>
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="https://nickify.vercel.app"
-            className="flex flex-col gap-1.5"
-          >
-            <h1 className="font-medium underline underline-offset-4">
-              nickify
-            </h1>
-            <p className="text-sm text-muted-foreground">
-              in one command, you can make your subscriber count visible in your
-              nickname.
-            </p>
-          </Link>
-        </li>
+        <Project
+          name="yabcounts"
+          description="livecounts, reimagined."
+          href="https://yabcounts.com"
+        />
+        <Project
+          name="statistical"
+          description="tracking your favorite creators so you can see their growth."
+          href="https://statistical.vercel.app"
+        />
+        <Project
+          name="nickify"
+          description="add your subscriber count to your nickname with ease."
+          href="https://nickify.vercel.app"
+        />
       </ul>
     </>
   );
